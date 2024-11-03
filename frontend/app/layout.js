271b0ef4b035d7b4f468ff30294f357a09526c03
ArekHanejko <arekimmobile@gmail.com>
2024-11-03 15:02:28 +0100

    HOC added, and modyfied style config files

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-[100%]"> 
+    <html lang="en" className="bg-white h-[100%]"> 
       <body className="h-[100%]">
         <UserProvider>
           <Menu />

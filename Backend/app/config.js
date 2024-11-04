d9343e0bd25815521f1a96af27cb73835ca202a8
AlbertK10 <103700277+AlbertK10@users.noreply.github.com>
2024-11-04 16:24:35 +0100

    Update config.js
    Added link to the database

--- Backend/app/config.js
+++ Backend/app/config.js
@@ -1,6 +1,6 @@
 const config = {
     port: process.env.PORT || 3001,
-    databaseUrl: process.env.MONGODB_URI || '',
+    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://zi_projekt:L7izN7BEN59yVoN1@zicluster.hic5b.mongodb.net/?retryWrites=true&w=majority&appName=ZICluster',
     JwtSecret: process.env.JWT_SECRET || 'secret'
 };
 export default config;
